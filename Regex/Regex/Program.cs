5f9f7e9346a0e75126ffb67cb32a5f2f7070ad67
Maronda Anderson-Johnson <66698985+Maronda12@users.noreply.github.com>
2021-08-30 18:12:56 -0400

    Add files via upload

---
+++ Regex/Regex/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegexLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            //1. Write a method that will validate names. Names can only have alphabets, they should start with a capital letter, and have a maximum of length of 30.
+
+            Regex name = new Regex("^[A-Z]{1}[a-z]{1,29}$");
+            string input;
+
+            Console.WriteLine("Enter your name: ");
+            input = Console.ReadLine();
+
+            while (name.Match(input).Success == false)
+            {
+                Console.WriteLine("Invalid. Please enter your name. ");
+                input = Console.ReadLine();
+            }
+            Console.WriteLine("Validated!");
+
+            
+        }
+        
+    }
+    
+}
